2f4432d931f443c416e1e50132aefb7ec394f760
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-13 21:06:43 -0600

    began work on making logo move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,65 @@
-console.log('Script connected.');
+const velocity = 0.025;
 
 class Logo {
     constructor(logoEl) {
         this.logoEl = logoEl;
+        this.reset();
     };
 
-    testMethod() {
-        console.log('testing...');
-        console.log(this.logoEl);
+    // get x value from stylesheet
+    get x() {
+        return parseFloat(getComputedStyle(this.logoEl).getPropertyValue('--x'));
+    };
+
+    // set x value on stylesheet
+    set x(value) {
+        this.logoEl.style.setProperty('--x', value);
+    };
+
+    // get y value from stylesheet
+    get y() {
+        return parseFloat(getComputedStyle(this.logoEl).getPropertyValue('--y'));
+    };
+
+    // set y value on stylesheet
+    set y(value) {
+        this.logoEl.style.setProperty('--y', value);
+    };
+
+    rect() {
+        return this.logoEl.getBoundingClientRect();
+    };
+
+    reset() {
+        this.x = 50;
+        this.y = 50;
+        this.direction = { x: 0 };
+        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+            const heading = Math.random() * (2 * Math.PI);
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+        };
+        this.velocity = velocity;
+    };
+
+    // move the logo
+    update(delta) {
+        this.x += this.direction.x * this.velocity * delta;
+        this.y += this.direction.y * this.velocity * delta;
     };
 }
 
 const logo = new Logo(document.querySelector('.dvd-logo'));
 
-logo.testMethod();
+let prevTime;
+const update = time => {
+    if (prevTime !== null) {
+        // set up delta as the difference in time since the last update was called
+        const delta = time - prevTime;
+        logo.update(delta);
+    };
+
+    prevTime = time;
+    window.requestAnimationFrame(update);
+};
+
+window.requestAnimationFrame(update);
